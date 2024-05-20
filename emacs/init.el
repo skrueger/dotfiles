847ed469b53b0c9d57e3177c36d29d082c8ac4c4
Simon Krueger <simonpkrueger@gmail.com>
2024-05-20 10:19:50 -0700

    Ask to exit emacs
    This helps prevent me from accidentally exiting emacs when i do :wq or
    Shift-Z-Z.

--- emacs/init.el
+++ emacs/init.el
@@ -139,6 +139,7 @@
    backup-by-copying t      ; don't clobber symlinks
    backup-directory-alist
     '(("." . "~/.saves/"))    ; don't litter my fs tree
+   confirm-kill-emacs #'yes-or-no-p
    delete-old-versions t
    kept-new-versions 6
    kept-old-versions 2
